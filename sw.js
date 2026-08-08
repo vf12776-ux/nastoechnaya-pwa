bde7cc1955451648109dc13ae30198a04baa7b74
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-08-08 13:36:50 +0300

    Transparent glass shot glass with burgundy liquid, bump to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nastoechnaya-v4';
+const CACHE_NAME = 'nastoechnaya-v5';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png', '/offline.html'];
 
 self.addEventListener('install', event => {
