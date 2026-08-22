17f7ad91c755452ffb3559f7e18f20516cbc244e
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-08-22 15:24:36 +0300

    Fix Yandex install: simple hint + reload, add meta tags and shortcuts, bump to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nastoechnaya-v7';
+const CACHE_NAME = 'nastoechnaya-v9';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png', '/offline.html'];
 
 self.addEventListener('install', event => {
